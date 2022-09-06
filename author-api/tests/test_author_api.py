97d587f3a8f38189edbf2cdbcfd3fe7b0580b44f
tbegum17 <tbegum107@gmail.com>
2022-09-06 15:11:04 +0000

    Testing

---
+++ author-api/tests/test_author_api.py
@@ -0,0 +1,21 @@
+from flask import url_for
+from flask_testing import TestCase
+from unittest.mock import patch
+import pytest
+# import the app's classes and objects
+from application import app
+
+# Create the base class
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+
+class TestViews(TestBase):
+
+    def test_get_unit(self):
+       with patch('random.choice') as r:
+           r.return_value = "Ana Huang"
+           response = self.client.get(url_for('author'))
+           self.assertEqual(response.status_code, 200)
+           self.assertIn(b'Ana Huang', response.data)
