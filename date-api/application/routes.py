9c9f4c8bc7088b73e3c8de8ac5edd19375f47cd8
tbegum17 <tbegum107@gmail.com>
2022-09-07 12:55:05 +0000

    Amended App

--- date-api/application/routes.py
+++ date-api/application/routes.py
@@ -5,8 +5,4 @@ import random
 
 @app.route('/get_book_date', methods=['POST'])
 def date():
-    #book_data = request.get_json()
-    #book_name = book_data["Book Name"]
-    #book_author = book_data["Book Author"]
-    date = random.choice(["2012-06-08", "2016-11-29", "2022-04-14"])
-    return Response(date, mimetype='text/plain')
+    return Response(random.choice(["2012-06-08", "2016-11-29", "2022-04-14"]), mimetype='text/plain')
